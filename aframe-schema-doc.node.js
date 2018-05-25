b155c1d3d40ff63aa03a95c2393f02e86c484eb0
harlyq <celerity252@gmail.com>
2018-05-25 23:17:04 +0930

    0.6.0

--- aframe-schema-doc.node.js
+++ aframe-schema-doc.node.js
@@ -2,8 +2,7 @@
 // THIS CODE IS AUTOMATICALLY GENERATED USING build.js
 
 // NODE env - setup jsdom and determine the filename to use
-const isNode = typeof process === "object" && Object.prototype.toString.call(process) === "[object process]"
-if (isNode) {
+if (typeof process === "object" && Object.prototype.toString.call(process) === "[object process]") {
   const jsdom = require("jsdom")
   const { JSDOM } = jsdom
    
@@ -18,14 +17,31 @@ if (isNode) {
   global.document = window.document
   
   global.AFRAME = require("aframe/src")
+
+  config = {sort: true, kebabCase: true}
+
+  for (let i = 2, n = process.argv.length; i < n; i++) {
+    switch(process.argv[i].toLowerCase()) {
+      case "--unsorted": config.sort = false; console.log("disable sort"); break
+      case "--camelcase": config.kebabCase = false; console.log("output camel case"); break
+      case "--sorted": config.sort = true; console.log("enable sort"); break
+      case "--kebabcase": config.kebabCase = true; console.log("output kebab case"); break
+      default: 
+        if (process.argv[i].includes("--")) {
+          console.error(`unknown switch: '${process.argv[i]}'`)
+        } else {
+          filename = process.argv[i]
+        }
+    }
+  }
 }
 
 
-(function(AFRAME, log) {
+(function(AFRAME, log, config) {
   let ARG_SORT = true
 
   // translate the schema into a markdown string
-  function schemaToMarkdown(type, name, schema) {
+  function schemaToMarkdown(type, name, schema, config) {
     let str = `
 ## ${type} ${name}
 | Property | Description | Default Value | Type |
@@ -99,17 +115,18 @@ if (isNode) {
     }   
 
     let schemaKeys = Object.keys(schema)
-    if (ARG_SORT) {
+    if (config.sort) {
       schemaKeys.sort()
     }
 
     for (let key of schemaKeys) {
+      let attr = config.kebabCase ? toKebabCase(key) : key
       let property = schema[key]
       let description = printIf(property.if) + property.description
       let def = typeof property.default !== "undefined" ? printValue(property.default) : printDefaultFromType(property.type)
       let type = property.oneOf ? printValue(property.oneOf, false) : (property.type ? property.type : printTypeFromDefault(property.default))
 
-      str += `\n|${toKebabCase(key)}|${description}|${def}|${type}|`
+      str += `\n|${attr}|${description}|${def}|${type}|`
     }
 
     return str
@@ -118,25 +135,25 @@ if (isNode) {
   // replace the AFRAME register functions with our output function
   const oldRegisterComponent = AFRAME.registerComponent
   AFRAME.registerComponent = function(name, options) {
-    log(schemaToMarkdown("Component", name, options.schema))
+    log(schemaToMarkdown("Component", name, options.schema, config))
     oldRegisterComponent(name, options)
   }
 
   const oldRegisterSystem = AFRAME.registerSystem
   AFRAME.registerSystem = function(name, options) {
-    log(schemaToMarkdown("System", name, options.schema))
+    log(schemaToMarkdown("System", name, options.schema, config))
     oldRegisterSystem(name, components)
   }
 
-})(AFRAME, args => console.log(args))
+})(AFRAME, args => console.log(args), config || {sort: true, kebabCase: true})
 
 // NODE env - load the file 
-if (isNode) {
+if (typeof process === "object" && Object.prototype.toString.call(process) === "[object process]") {
   const path = require("path")
 
   // switch to friendly slashes and ensure relative to the current directory
-  const filename = process.argv[2]
-  console.assert(filename, `first parameter should be a .js filename to parse`);
+  // const filename = process.argv[2]
+  console.assert(filename, `missing .js filename to parse`);
   
   let safeFilename = filename.replace("\\", "/")
   if (!path.isAbsolute(filename)) {
